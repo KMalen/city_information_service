9e5a3279072d83ca53442cc0eeb434e78f2fd390
Kiryl Malenchik <kirillmalen@gmail.com>
2020-10-21 00:32:13 +0300

    Initial Commit

---
+++ city_information_service/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  city_information_service
+//
+//  Created by Kiril Malenchik on 10/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
